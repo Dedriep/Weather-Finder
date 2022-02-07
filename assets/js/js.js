95b498422fb24ea0e4b0c05521eb757ea3fbfd49
dedrie p <dedrie@dedries-MacBook-Air.local>
2022-02-07 01:47:40 -0500

    updated one call api

--- assets/js/js.js
+++ assets/js/js.js
@@ -3,8 +3,7 @@ var apiKey = 'e25813769e73c32b981b464f3ca85e2c';
 console.log("loading js script file ...");
 var searchButton = document.getElementById("submit-button");
 var searchInput = document.getElementById("search-input");
-var latitude; 
-var longitude; 
+
 
 //Declaring a Function 
 function getWeather(event) {
@@ -14,35 +13,58 @@ function getWeather(event) {
 
     var locationURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + searchInput.value + '&limit=1&appid=' + apiKey;
     console.log(locationURL);
+    
 
     //Fetching the lat and Lon based on the search input entered by the user 
     fetch(locationURL)
+    
         .then(function (res) {
             //console.log("Response", res);
             return res.json(); //returns a JSON friendly response on a successful api call
         }).then(function (apiData) {
             console.log("Api JSON Response", apiData);
+            
             //GRAB THE LAT AND LON from the response 
-            latitude = apiData[0].lat; 
-            longitude = apiData[0].lon; 
-            console.log("lat", latitude);
+            var latitude = apiData[0].lat; 
+            var longitude = apiData[0].lon; 
+            
+            console.log("lat", longitude, latitude);
             //display the city on the HTML 
-            document.getElementById("current-city").textContent = apiData[0].name;            // CALL THE ONE CALL API fetch request 
-            oneCallAPIWeather(latitude, longitude); 
+            document.getElementById("current-city").textContent = apiData[0].name;            
+            
+            // CALL THE ONE CALL API fetch request 
+            //oneCallAPIWeather(latitude, longitude); 
+            //console.log (oneCallAPIWeather(latitude, longitude))
 
         }).catch(function (error) {
             console.log("Error Msg", error)
         })
+
+   
+
 
     //Reset the search input field 
     searchInput.value = "";
 }
 
 function oneCallAPIWeather(latitude, longitude){
-    //URL variable 
 
+    //URL variable 
+    var oneCallAPIWeather = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + 'exclude=alerts&appid=' + apiKey
+        console.log(oneCallAPIWeather)
 
     //FETCH REQUEST 
+    fetch (oneCallAPIWeather)
+        .then (function (response) {
+
+            return response.json();
+        }).then (function (apiData) {
+            console.log("api data", apiData);
+            
+        }).catch(function (error) {
+            console.log("Error Msg", error)
+        });
+
 
     //DISPLAY IT ON THE HTML PAGE 
     
